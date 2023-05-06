7ff8d29afe19cd080ecf0422d20a79d77e8a93f6
ceeyIT <104657912+ceeyIT@users.noreply.github.com>
2023-05-06 14:06:44 -0500

    Update install_ansible.sh

--- install_ansible.sh
+++ install_ansible.sh
@@ -2,6 +2,6 @@
 sudo yum -y install epel-release
 sudo yum -y install ansible
 sudo yum -y install git
-git clone https://github.com/ravi2krishna/tf-multiple-mdp.git /home/centos
-cd tf-multiple-mdp
+git clone https://github.com/CeeyIT-Solutions/batch1-tf.git/home/centos
+cd batch1-tf
 ansible-playbook setup_ecomm.yml -i aws.ini
